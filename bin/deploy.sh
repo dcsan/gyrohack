d1d04fa3c8e7c2d5a18135ca4e827d6cfc370bb4
dcsan <dc@pikkle.com>
2014-09-14 03:12:47 -0700

    big buttons

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,11 +1,13 @@
 
-cd app && meteor deploy gyrotanks.meteor.com
+TGT="gyrotanks.meteor.com"
+cd app && meteor deploy $TGT
+
 
 ## after cd app we have to cd back?
 cd -
 
 # give meteor time to restart
-sleep 5
-
+sleep 10
+curl "${TGT}/reset"
 
 meteor logs redes.meteor.com
